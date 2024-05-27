06d5de6ba8c70aceb54ec4dbb283f5543b37635a
Dalton Sevier <daltonsevier@gmail.com>
2024-05-27 17:54:57 -0400

    Implement Landing Page

--- src/sections/SpecialOffer.jsx
+++ src/sections/SpecialOffer.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import Button from '../components/Button'
+import { offer } from '../assets/images'
+import { arrowRight } from '../assets/icons'
 
 const SpecialOffer = () => {
   return (
-    <div>SpecialOffer</div>
+    <section className='flex justify-wrap items-center max-xl:flex-col-reverse gap-10 max-container'>
+      <div className='flex-1'>
+        <img src={offer} width={773} height={687} className='object-contain w-full' />
+      </div>
+      <div className='flex flex-1 flex-col'>
+        <h2 className=' font-palanquin text-4xl capitalize font-bold lg:max-w-lg'>
+          <span className='text-coral-red'>Special </span> Offer
+        </h2>
+        <p className='mt-4 lg:max-w-lg info-text'>
+          Embark on a shoppping journey...
+        </p>
+        <p className='mt-6 lg:max-w-lg info-text'></p>
+
+        <div className='mt-11 flex flex-wrap gap-4'>
+          <Button label='Shop Now' iconURL={arrowRight} />
+          <Button label='Learn More' backgroundColor='bg-white' borderColor='border-slate-gray' textColor='text-slat-gray'/>
+        </div>   
+      </div>
+    </section>
   )
 }
 
